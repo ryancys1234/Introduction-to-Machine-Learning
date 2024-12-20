a969571ed31e2a6ec7d203c4ff52c3f4747cca4d
Ryan Shi <55104016+ryancys1234@users.noreply.github.com>
2024-12-20 11:26:34 -0800

    Update logistic.py

--- Assignment2/q3/logistic.py
+++ Assignment2/q3/logistic.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def logistic_predict(weights, data):
     N = len(data)
-    X = np.c_[data, np.ones(N)] # Adds the dummy feature to the data for bias
+    X = np.c_[data, np.ones(N)]
     z = np.dot(X, weights)
     y = sigmoid(z)
     return y
@@ -11,14 +11,14 @@ def logistic_predict(weights, data):
 def evaluate(targets, y):
     N = len(targets)
     sum = 0
-    for i in range(N): # Note: (targets[i]-1) and -(1-targets[i]) doesn't work due to an encountered uint8 bug
+    for i in range(N): # Note: (targets[i]-1) and -(1-targets[i]) doesn't work due to an uint8 bug
         t = targets[i]
         y_i = y[i]
         sum -= t*np.log(y_i)
         sum -= (1-t)*np.log(1-y_i)
     
     ce = float(sum / N)
-    frac_correct = np.mean([int( targets[i] == int(y[i] >= 0.5) ) for i in range(N)]) # Computes the fraction using np.mean()
+    frac_correct = np.mean([int( targets[i] == int(y[i] >= 0.5) ) for i in range(N)]))
     
     # Debugging code:
     # train_targets = targets; i = np.random.randint(N-19, N)
@@ -32,7 +32,7 @@ def evaluate(targets, y):
 def logistic(weights, data, targets, hyperparameters):
     y = logistic_predict(weights, data)
     N = len(targets)
-    X = np.c_[data, np.ones(N)] # Adds the dummy feature to the data for bias
-    f = evaluate(targets, y)[0] # The averaged cross entropy loss
+    X = np.c_[data, np.ones(N)]
+    f = evaluate(targets, y)[0]
     df = (1/N)*X.T.dot([y_i - t_i for y_i, t_i in zip(y, targets)])
     return f, df, y
