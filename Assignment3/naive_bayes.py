7016963282e001e379bbffa705e6b1f55d0a5e14
Ryan Shi <55104016+ryancys1234@users.noreply.github.com>
2024-12-20 11:41:12 -0800

    Update naive_bayes.py

--- Assignment3/naive_bayes.py
+++ Assignment3/naive_bayes.py
@@ -1,12 +1,5 @@
-import numpy as np
-import os
-import gzip
-import struct
-import array
-import matplotlib.pyplot as plt
-import matplotlib.image
 from urllib.request import urlretrieve
-
+import array, gzip, matplotlib.image, matplotlib.pyplot as plt, numpy as np, os, struct
 
 def download(url, filename):
     if not os.path.exists('data'):
@@ -15,10 +8,9 @@ def download(url, filename):
     if not os.path.isfile(out_file):
         urlretrieve(url, out_file)
 
-
 def mnist():
     base_url = 'http://yann.lecun.com/exdb/mnist/'
-
+    
     def parse_labels(filename):
         with gzip.open(filename, 'rb') as fh:
             magic, num_data = struct.unpack(">II", fh.read(8))
@@ -29,19 +21,14 @@ def mnist():
             magic, num_data, rows, cols = struct.unpack(">IIII", fh.read(16))
             return np.array(array.array("B", fh.read()), dtype=np.uint8).reshape(num_data, rows, cols)
 
-    for filename in ['train-images-idx3-ubyte.gz',
-                     'train-labels-idx1-ubyte.gz',
-                     't10k-images-idx3-ubyte.gz',
-                     't10k-labels-idx1-ubyte.gz']:
+    for filename in ['train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz']:
         download(base_url + filename, filename)
 
     train_images = parse_images('data/train-images-idx3-ubyte.gz')
     train_labels = parse_labels('data/train-labels-idx1-ubyte.gz')
     test_images = parse_images('data/t10k-images-idx3-ubyte.gz')
     test_labels = parse_labels('data/t10k-labels-idx1-ubyte.gz')
-
     return train_images, train_labels, test_images[:1000], test_labels[:1000]
-
 
 def load_mnist():
     partial_flatten = lambda x: np.reshape(x, (x.shape[0], np.prod(x.shape[1:])))
@@ -52,18 +39,15 @@ def load_mnist():
     train_labels = one_hot(train_labels, 10)
     test_labels = one_hot(test_labels, 10)
     N_data = train_images.shape[0]
-
     return N_data, train_images, train_labels, test_images, test_labels
 
-
-def plot_images(images, ax, ims_per_row=5, padding=5, digit_dimensions=(28, 28),
-                cmap='binary', vmin=None, vmax=None):
-    """Images should be a (N_images x pixels) matrix."""
+def plot_images(images, ax, ims_per_row=5, padding=5, digit_dimensions=(28, 28), cmap='binary', vmin=None, vmax=None):
     N_images = images.shape[0]
     N_rows = np.int32(np.ceil(float(N_images) / ims_per_row))
     pad_value = np.min(images.ravel())
     concat_images = np.full(((digit_dimensions[0] + padding) * N_rows + padding,
                              (digit_dimensions[1] + padding) * ims_per_row + padding), pad_value)
+    
     for i in range(N_images):
         cur_image = np.reshape(images[i, :], digit_dimensions)
         row_ix = i // ims_per_row
@@ -75,8 +59,8 @@ def plot_images(images, ax, ims_per_row=5, padding=5, digit_dimensions=(28, 28),
         cax = ax.matshow(concat_images, cmap=cmap, vmin=vmin, vmax=vmax)
         plt.xticks(np.array([]))
         plt.yticks(np.array([]))
+    
     return cax
-
 
 def save_images(images, filename, **kwargs):
     fig = plt.figure(1)
@@ -87,80 +71,48 @@ def save_images(images, filename, **kwargs):
     ax.patch.set_visible(False)
     plt.savefig(filename)
 
-
 def train_mle_estimator(train_images, train_labels):
-    """ Inputs: train_images, train_labels
-        Returns the MLE estimators theta_mle and pi_mle"""
-
-    n_class = np.ndarray.sum(train_labels, axis=0) # Number of labels in each class
-    theta_mle = np.dot(train_images.T, train_labels) / n_class # MLE estimate of theta
-    pi_mle = n_class / train_images.shape[0] # MLE estimate of pi
-    
+    n_class = np.ndarray.sum(train_labels, axis=0)
+    theta_mle = np.dot(train_images.T, train_labels) / n_class
+    pi_mle = n_class / train_images.shape[0]
     return theta_mle, pi_mle
 
-
 def train_map_estimator(train_images, train_labels):
-    """ Inputs: train_images, train_labels
-        Returns the MAP estimators theta_map and pi_map"""
-    
-    n_class = np.ndarray.sum(train_labels, axis=0) # Number of labels in each class
-    theta_map = (np.dot(train_images.T, train_labels) + 2) / (n_class + 4) # MAP estimate of theta
-    pi_map = n_class / train_images.shape[0] # MAP estimate of pi
-    
+    n_class = np.ndarray.sum(train_labels, axis=0)
+    theta_map = (np.dot(train_images.T, train_labels) + 2) / (n_class + 4)
+    pi_map = n_class / train_images.shape[0]
     return theta_map, pi_map
 
-
 def log_likelihood(images, theta, pi):
-    """ Inputs: images, theta, pi
-        Returns the matrix 'log_like' of loglikehoods over the input images where
-    log_like[i,c] = log p (c |x^(i), theta, pi) using the estimators theta and pi.
-    log_like is a matrix of num of images x num of classes
-    Note that log likelihood is not only for c^(i), it is for all possible c's."""
-
     p_x = np.dot(images, np.log(theta)) + np.dot((1-images), np.log(1-theta))
     log_like = p_x - np.log(np.dot(pi, np.exp(p_x).T))[:,np.newaxis] + np.log(pi)
     return log_like
 
-
 def predict(log_like):
-    """ Inputs: matrix of log likelihoods
-    Returns the predictions based on log likelihood values"""
-
-    predictions = np.argmax(log_like, axis=1) # List of indices of max log-likehood for each image
+    predictions = np.argmax(log_like, axis=1)
     return predictions
 
-
 def accuracy(log_like, labels):
-    """ Inputs: matrix of log likelihoods and 1-of-K labels
-    Returns the accuracy based on predictions from log likelihood values"""
-
     acc = 0
     preds = predict(log_like)
-    
     for i in range(len(labels)):
         acc += labels[i][preds[i]]
-    
     acc = acc / len(labels)
-    
     return acc
 
-
-# +
 def main():
     N_data, train_images, train_labels, test_images, test_labels = load_mnist()
     
-    # Fit MLE and MAP estimators
     theta_mle, pi_mle = train_mle_estimator(train_images, train_labels)
     theta_map, pi_map = train_map_estimator(train_images, train_labels)
 
-    # Find the log likelihood of each data point
-#     loglike_train_mle = log_likelihood(train_images, theta_mle, pi_mle)
+    # loglike_train_mle = log_likelihood(train_images, theta_mle, pi_mle)
     loglike_train_map = log_likelihood(train_images, theta_map, pi_map)
 
-#     avg_loglike_mle = np.sum(loglike_train_mle * train_labels) / N_data
+    # avg_loglike_mle = np.sum(loglike_train_mle * train_labels) / N_data
     avg_loglike_map = np.sum(loglike_train_map * train_labels) / N_data
 
-#     print("Average log-likelihood for MLE is ", avg_loglike_mle)
+    # print("Average log-likelihood for MLE is ", avg_loglike_mle)
     print("Average log-likelihood for MAP is ", avg_loglike_map)
 
     train_accuracy_map = accuracy(loglike_train_map, train_labels)
@@ -170,7 +122,6 @@ def main():
     print("Training accuracy for MAP is ", train_accuracy_map)
     print("Test accuracy for MAP is ", test_accuracy_map)
 
-    # Plot MLE and MAP estimators
     save_images(theta_mle.T, 'mle.png')
     save_images(theta_map.T, 'map.png')
 
