0d1e689d2f6b89002db6f121aa611630bded39b4
Ryan Shi <55104016+ryancys1234@users.noreply.github.com>
2024-12-20 11:29:48 -0800

    Update logistic.py

--- Assignment2/q3/logistic.py
+++ Assignment2/q3/logistic.py
@@ -11,13 +11,11 @@ def logistic_predict(weights, data):
 def evaluate(targets, y):
     N = len(targets)
     sum = 0
-    
     for i in range(N): # Note: (targets[i]-1) and -(1-targets[i]) doesn't work due to an uint8 bug
         t = targets[i]
         y_i = y[i]
         sum -= t*np.log(y_i)
         sum -= (1-t)*np.log(1-y_i)
-    
     ce = float(sum / N)
     frac_correct = np.mean([int( targets[i] == int(y[i] >= 0.5) ) for i in range(N)]))
     
